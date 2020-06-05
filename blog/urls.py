4c2abc4732ef0dd396a64b9f7cef54684d265a9e
Justyna Broniszewska <jj.broniszewska@gmail.com>
2020-06-05 16:08:05 +0200

    Django sitemap functionallity added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
 app_name = "blog"
+sitemaps = {
+    "posts": PostSitemap,
+}
+
 urlpatterns = [
     path("", views.PostList.as_view(), name="post_list"),
     path("tag/<slug:tag_slug>/", views.PostList.as_view(), name="post_list_by_tag"),
@@ -14,4 +20,10 @@ urlpatterns = [
         "<int:post_id>/udostepnij-post/", views.SharePost.as_view(), name="share_post"
     ),
     path("kontakt/", views.ContactUs.as_view(), name="contact"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
